bb29aa027ef63372ce1dda3d4e7406a4df98ca9d
Enzo Almeida <enzo.alverde@gmail.com>
2025-07-24 15:26:44 -0300

    fix: corrigindo rotas

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,14 +10,10 @@ app = FastAPI(
     description="API para gestão de fotos de promotores via WhatsApp.", 
     version="2.0.0"
 )
-VERCEL_PRODUCTION_URL = "mustafa-backend-enzoalmeida21s-projects.vercel.app"
-
-
 # Configuração do CORS (já estava correta)
 origins = [
     "http://localhost:3000",
-    f"https://{VERCEL_PRODUCTION_URL}",
-    f"https://www.{VERCEL_PRODUCTION_URL}",
+    "https://mustafa-backend-enzoalmeida21s-projects.vercel.app"
     "https://mustafa-backend-k4w21ohkh-enzoalmeida21s-projects.vercel.app"
 ]
 
